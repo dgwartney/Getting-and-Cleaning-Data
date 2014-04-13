f36454a71a69683190159295bad5720f340a78fb
dgwartney <david.gwartney@gmail.com>
2014-04-13 16:29:38 -0700

    Completion of homework #1

--- quiz1.R
+++ quiz1.R
@@ -17,11 +17,35 @@ sum(dat$Zip*dat$Ext,na.rm=T)
 
 library(XML)
 xmlFile <- "restaurants.xml"
-download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml",xmlFile,method="curl")
+download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml",destfile=xmlFile,method="curl")
 doc <- xmlTreeParse(xmlFile,useInternal=TRUE)
 rootNode <- xmlRoot(doc);
 xmlName(rootNode)
 rows <- xpathSApply(rootNode,"//zipcode",xmlValue)
 class(rows)
 length(rows[rows == 21231])
+
+library(data.table)
+csvFile="ss06pid.csv"
+download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv",destfile=csvFile,method="curl")
+DT <- fread(csvFile)
+
+print("*** first ***")
+system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+print("*** second ***")
+system.time(mean(DT[DT$SEX==1,]$pwgtp15))
+system.time(mean(DT[DT$SEX==2,]$pwgtp15))
+print("*** third ***")
+system.time(DT[,mean(pwgtp15),by=SEX])
+print("*** four ***")
+system.time(rowMeans(DT)[DT$SEX==1])
+system.time(rowMeans(DT)[DT$SEX==2])
+print("*** Five ***")
+system.time(mean(DT$pwgtp15,by=DT$SEX))
+print("*** Six ***")
+system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
+
+
+
+
 
